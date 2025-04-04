7726b78b690ecfd2ddc5e7e68f99c4fd6636004d
Luksa Kraljevic <luksa.kraljevic@wearenotch.com>
2025-04-04 09:17:58 +0200

    Query expansion

--- src/main/java/com/wearenotch/kluksa/notchchatbot/config/RAGConfig.java
+++ src/main/java/com/wearenotch/kluksa/notchchatbot/config/RAGConfig.java
@@ -1,7 +1,11 @@
 package com.wearenotch.kluksa.notchchatbot.config;
 
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.RetrievalAugmentationAdvisor;
+import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.ai.rag.generation.augmentation.ContextualQueryAugmenter;
+import org.springframework.ai.rag.preretrieval.query.expansion.MultiQueryExpander;
+import org.springframework.ai.rag.preretrieval.query.expansion.QueryExpander;
 import org.springframework.ai.rag.retrieval.search.VectorStoreDocumentRetriever;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.context.annotation.Bean;
@@ -10,9 +14,11 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RAGConfig {
     private final VectorStore vectorStore;
+    private final ChatClient.Builder chatClientBuilder;
 
-    public RAGConfig(VectorStore vectorStore) {
+    public RAGConfig(VectorStore vectorStore, ChatClient.Builder chatClientBuilder) {
         this.vectorStore = vectorStore;
+        this.chatClientBuilder = chatClientBuilder;
     }
 
     @Bean
@@ -22,9 +28,36 @@ public class RAGConfig {
                 .similarityThreshold(0.50)
                 .vectorStore(vectorStore)
                 .build())
+            .queryExpander(queryExpander())
             .queryAugmenter(ContextualQueryAugmenter.builder()
                 .allowEmptyContext(true)
                 .build())
+            .build();
+    }
+
+    private QueryExpander queryExpander() {
+        String templateString = """
+            You are an expert at information retrieval and search optimization.
+            Your task is to generate {number} different versions of the given query.
+            
+            If the original query is composed of multiple questions, put each question in a separate variant.
+            Each variant must cover different perspectives or aspects of the topic,
+            while maintaining the core intent of the original query. The goal is to
+            expand the search space and improve the chances of finding relevant information.
+            
+            Do not explain your choices or add any other text.
+            Provide the query variants separated by newlines.
+            
+            Original query: {query}
+            
+            Query variants:
+            """;
+
+        return MultiQueryExpander.builder()
+            .chatClientBuilder(chatClientBuilder)
+            .includeOriginal(true)
+            .promptTemplate(new PromptTemplate(templateString))
+            .numberOfQueries(4)
             .build();
     }
 }
